9ca104b1d23155e6179875e11aa5c92ee87903da
Kelvinwhan <kelvinwhan@gmail.com>
2026-08-24 21:46:38 +0800

    Update jc4827_display.cpp

--- components/jc4827/jc4827_display.cpp
+++ components/jc4827/jc4827_display.cpp
@@ -1,22 +1,18 @@
-#include "jc4827_display.h"
+#include "esphome/core/component.h"
 
-void JC4827Display::setup() {
-  bus = new Arduino_ESP32RGBPanel(
-    40, 39, 41, 42,
-    45, 48, 47, 21, 14,
-    5, 6, 7, 15, 16, 4,
-    8, 3, 46, 9, 1
-  );
-  gfx = new Arduino_RGB_Display(bus, 480, 272);
-  gfx->begin();
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
-}
+namespace esphome {
+namespace jc4827 {
 
-void JC4827Display::update() {
-  // LVGL will handle drawing
-}
+class JC4827 : public Component {
+ public:
+  void setup() override {
+    // initialization code
+  }
 
-void JC4827Display::draw_pixel_at(int x, int y, esphome::Color color) {
-  gfx->drawPixel(x, y, color.to_rgb_565());
-}
+  void loop() override {
+    // main loop code
+  }
+};
+
+}  // namespace jc4827
+}  // namespace esphome
