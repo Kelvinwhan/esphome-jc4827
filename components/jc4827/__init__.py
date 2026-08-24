befde33076183671c13b79c0afc7f9c7c9330ac4
Kelvinwhan <kelvinwhan@gmail.com>
2026-08-24 21:29:48 +0800

    Fix import for configuration variable in init.py

--- components/jc4827/__init__.py
+++ components/jc4827/__init__.py
@@ -1,18 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import core
 
-# Namespace for your component
 jc4827_ns = cg.esphome_ns.namespace('jc4827')
 JC4827 = jc4827_ns.class_('JC4827', cg.Component)
 
-# Define YAML schema
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(JC4827),
-    cv.Optional("display"): cv.use_id(JC4827),
 })
 
-# Code generation hook
 async def to_code(config):
-    var = cg.new_Pvariable(config[core.CONF_ID])
+    var = cg.new_Pvariable(config[cv.CONF_ID])
     await cg.register_component(var, config)
