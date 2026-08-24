acb35073cf51f4b84f388160697747363836ee52
Kelvinwhan <kelvinwhan@gmail.com>
2026-08-25 00:06:40 +0800

    Clarify library linking comment in __init__.py
    Updated comment for clarity on library linking.

--- components/jc4827/__init__.py
+++ components/jc4827/__init__.py
@@ -16,5 +16,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[cv.CONF_ID])
     await display.register_display(var, config)
     
-    # Download and link TouchLib repository without appending @None
+    # Passing "" prevents ESPHome from appending @None to the URL
     cg.add_library("https://github.com/mmicko/TouchLib.git", "")
