8389c1465ad0e3cd7519a027434deca42e7ca976
Kelvin Whan <kelvinwhan@gmail.com>
2026-08-24 21:42:26 +0800

    Add JC4827 C++ stub class

---
+++ components/jc4827/jc4827.cpp
@@ -0,0 +1,18 @@
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace jc4827 {
+
+class JC4827 : public Component {
+ public:
+  void setup() override {
+    // initialization code
+  }
+
+  void loop() override {
+    // main loop code
+  }
+};
+
+}  // namespace jc4827
+}  // namespace esphome
