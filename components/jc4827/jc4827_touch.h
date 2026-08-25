91b82122b2551bec6796f88054af9d03482ac2c3
Kelvinwhan <kelvinwhan@gmail.com>
2026-08-25 22:51:24 +0800

    Refactor JC4827Touch to use GPIO pins
    Refactor JC4827Touch class to use GPIO pins and remove dependency on DisplayBuffer.

--- components/jc4827/jc4827_touch.h
+++ components/jc4827/jc4827_touch.h
@@ -1,22 +1,28 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/display/display_buffer.h"
-#include "TouchLib.h"
+#include "esphome/core/gpio.h"
 
 namespace esphome {
 namespace jc4827 {
 
+class JC4827Display;  // forward declaration
+
 class JC4827Touch : public Component {
  public:
-  explicit JC4827Touch(DisplayBuffer *disp);
-
   void setup() override;
   void loop() override;
 
- private:
-  DisplayBuffer *display;
-  TouchLib *touch;
+  void set_display(JC4827Display *disp) { display_ = disp; }
+  void set_cs_pin(GPIOPin *pin) { cs_pin_ = pin; }
+  void set_irq_pin(GPIOPin *pin) { irq_pin_ = pin; }
+  void set_reset_pin(GPIOPin *pin) { reset_pin_ = pin; }
+
+ protected:
+  JC4827Display *display_{nullptr};
+  GPIOPin *cs_pin_{nullptr};
+  GPIOPin *irq_pin_{nullptr};
+  GPIOPin *reset_pin_{nullptr};
 };
 
 }  // namespace jc4827
