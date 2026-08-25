6880c864402737ecb39d094b5ef7dba444e7c97d
Kelvinwhan <kelvinwhan@gmail.com>
2026-08-25 22:18:50 +0800

    Refactor JC4827 display component initialization

--- components/jc4827/__init__.py
+++ components/jc4827/__init__.py
@@ -1,17 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import display
+from esphome.const import CONF_ID
 
-DEPENDENCIES = ["i2c"]
-AUTO_LOAD = ["display", "i2c"]
+jc4827_ns = cg.esphome_ns.namespace('jc4827')
+JC4827Display = jc4827_ns.class_('JC4827Display', display.DisplayBuffer)
+JC4827Touch = jc4827_ns.class_('JC4827Touch', cg.Component)
 
-jc4827_ns = cg.esphome_ns.namespace("jc4827")
-JC4827 = jc4827_ns.class_("JC4827", display.DisplayBuffer)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(JC4827Display),
+})
 
-CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(JC4827),
-}).extend(cv.COMPONENT_SCHEMA)
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[cv.CONF_ID])
-    await display.register_display(var, config)
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield display.register_display(var, config)
