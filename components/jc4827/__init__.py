5316b4c76506680e05dd69c3febfea5328060ec8
Kelvinwhan <kelvinwhan@gmail.com>
2026-08-25 22:29:34 +0800

    Update __init__.py

--- components/jc4827/__init__.py
+++ components/jc4827/__init__.py
@@ -7,6 +7,7 @@ jc4827_ns = cg.esphome_ns.namespace('jc4827')
 JC4827Display = jc4827_ns.class_('JC4827Display', display.DisplayBuffer)
 JC4827Touch = jc4827_ns.class_('JC4827Touch', cg.Component)
 
+# Schema for display
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(JC4827Display),
 })
