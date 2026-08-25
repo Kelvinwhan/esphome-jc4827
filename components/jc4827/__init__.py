aba50cba6afc6c51342afa5d297a641b5c5b9500
Kelvinwhan <kelvinwhan@gmail.com>
2026-08-25 22:48:21 +0800

    Implement touch schema for JC4827 component
    Added touch schema and related functions for JC4827.

--- components/jc4827/__init__.py
+++ components/jc4827/__init__.py
@@ -6,7 +6,9 @@ from esphome import pins
 
 jc4827_ns = cg.esphome_ns.namespace("jc4827")
 JC4827Display = jc4827_ns.class_("JC4827Display", display.DisplayBuffer)
+JC4827Touch = jc4827_ns.class_("JC4827Touch", cg.Component)
 
+# --- Display schema ---
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(JC4827Display),
     cv.Optional("cs_pin"): pins.gpio_output_pin_schema,
@@ -32,3 +34,28 @@ def to_code(config):
     if "backlight_pin" in config:
         bl = yield cg.gpio_pin_expression(config["backlight_pin"])
         cg.add(var.set_backlight_pin(bl))
+
+# --- Touch schema ---
+CONFIG_SCHEMA_TOUCH = cv.Schema({
+    cv.GenerateID(): cv.declare_id(JC4827Touch),
+    cv.Required("display"): cv.use_id(JC4827Display),
+    cv.Optional("cs_pin"): pins.gpio_output_pin_schema,
+    cv.Optional("irq_pin"): pins.gpio_input_pin_schema,
+    cv.Optional("reset_pin"): pins.gpio_output_pin_schema,
+})
+
+def to_code_touch(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    disp = yield cg.get_variable(config["display"])
+    cg.add(var.set_display(disp))
+    yield cg.register_component(var, config)
+
+    if "cs_pin" in config:
+        cs = yield cg.gpio_pin_expression(config["cs_pin"])
+        cg.add(var.set_cs_pin(cs))
+    if "irq_pin" in config:
+        irq = yield cg.gpio_pin_expression(config["irq_pin"])
+        cg.add(var.set_irq_pin(irq))
+    if "reset_pin" in config:
+        rst = yield cg.gpio_pin_expression(config["reset_pin"])
+        cg.add(var.set_reset_pin(rst))
