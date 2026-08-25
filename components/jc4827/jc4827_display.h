9486e95f5f6610ac9a4006fba529aec8b43bb540
Kelvinwhan <kelvinwhan@gmail.com>
2026-08-25 22:16:31 +0800

    Update jc4827_display.h

--- components/jc4827/jc4827_display.h
+++ components/jc4827/jc4827_display.h
@@ -1,13 +1,17 @@
 #pragma once
-#include "esphome/core/component.h"
+
+#include "esphome/components/display/display_buffer.h"
 
 namespace esphome {
 namespace jc4827 {
 
-class JC4827 : public Component {
+class JC4827Display : public display::DisplayBuffer {
  public:
   void setup() override;
   void loop() override;
+  void update() override;
+  int get_width() override { return 800; }   // adjust to your panel resolution
+  int get_height() override { return 480; }  // adjust to your panel resolution
 };
 
 }  // namespace jc4827
