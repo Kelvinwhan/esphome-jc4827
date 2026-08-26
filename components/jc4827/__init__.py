b3b4dd25893107b3b5e387ad72479213a101df0a
Kelvinwhan <kelvinwhan@gmail.com>
2026-08-26 16:30:13 +0800

    Fix typo in component registration yield statement
    new

--- components/jc4827/__init__.py
+++ components/jc4827/__init__.py
@@ -19,7 +19,7 @@ CONFIG_SCHEMA = cv.Schema({
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+    yield cg.register_component(var, config)jj
     yield display.register_display(var, config)
 
     if "cs_pin" in config:
